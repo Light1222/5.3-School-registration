f258a86abd84ff7a8cf5b5e75c5cf66fa4110326
r7qb <r7qb@fresnou.org>
2023-02-07 11:37:46 -0800

    Initial commit

---
+++ controllers/courseController.js
@@ -0,0 +1,18 @@
+const {Course} = require('../models')
+
+//view all
+module.exports.viewAll = async function (req, res){
+    const courses = await Course.findAll();
+    res.render('course/view_all', {Courses});
+}
+//profile
+
+//render add form
+
+//add
+
+//render edit form
+
+//update
+
+//delete
